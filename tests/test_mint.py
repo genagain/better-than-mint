5140573f76212aa8f747508f95e92ec4dbec5e8a
Gen <ohta.g@husky.neu.edu>
2019-09-13 00:46:37 -0400

    test mint integration

--- tests/test_mint.py
+++ tests/test_mint.py
@@ -1,11 +1,19 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
+import numpy as np
+import pandas as pd
 
 from betterthanmint import mint
 
-def test_get_transactions():
+def test_get_transactions(monkeypatch):
+    def mock_get_transactions():
+        transactions = pd.read_pickle('tests/transactions.pickle')
+        return transactions
+
+    monkeypatch.setattr(mint,'get_transactions', mock_get_transactions)
+
     transactions = mint.get_transactions()
-    # TODO assert no duplicates
-    # TODO assert columns
+
     assert type(transactions).__name__ == 'DataFrame'
+
+    expected_columns = np.array(['date', 'description', 'original_description', 'amount', 'transaction_type', 'category', 'account_name'], dtype=object)
+    assert np.all(transactions.columns.values == expected_columns)
+    assert np.all(transactions.duplicated(subset=['date','original_description','amount']) == False)
