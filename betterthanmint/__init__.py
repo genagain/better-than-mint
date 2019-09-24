fd9658deb7bb9de53b3e80f677cc323a7563e1ec
Gen <ohta.g@husky.neu.edu>
2019-09-24 19:45:54 -0400

    modify files to satisfy pylint

--- betterthanmint/__init__.py
+++ betterthanmint/__init__.py
@@ -1,3 +1,4 @@
+"""Entry point for the application script"""
 
 def main():
     """Entry point for the application script"""
--- betterthanmint/mint.py
+++ betterthanmint/mint.py
@@ -1,10 +1,12 @@
-import mintapi
+"""This module contains functions that interact with the unofficial mint package"""
 
 import os
+import mintapi
 
 def get_transactions():
+    """Returns a pd.DataFrame of deduplicated transactions stored in your Mint account"""
     mint = mintapi.Mint(os.environ['EMAIL'], os.environ['PASSWORD'])
     mint.initiate_account_refresh()
     transactions = mint.get_transactions()
-    transactions.drop_duplicates(['date','original_description','amount'], inplace=True)
+    transactions.drop_duplicates(['date', 'original_description', 'amount'], inplace=True)
     return transactions
